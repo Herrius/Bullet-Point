66814e07230697731f55406b2d910d16165b6099
herrius <enriqueubaldo97@gmail.com@>
2023-07-09 09:41:04 -0500

    add sounds

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -9,7 +9,10 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
     public int score;
     public TextMeshProUGUI scoreText;
-
+    public Camera mainCamera;
+    public GameObject turret;
+    public GameObject bulletPrefab;
+    public GameObject enemyPrefab;
 
     void Awake()
     {
@@ -25,6 +28,21 @@ public class ScoreManager : MonoBehaviour
     void Update()
     {
         scoreText.text = "Score: " + score;
+        if (score == 20)
+        {
+            mainCamera.orthographicSize = 7;
+            turret.transform.localScale = new Vector2(0.3f, 0.3f);
+            bulletPrefab.transform.localScale = new Vector2(0.17f, 0.09f);
+            enemyPrefab.transform.localScale = new Vector2(0.20f, 0.20f);
+        }
+
+        if (score == 40)
+        {
+            mainCamera.orthographicSize = 9;
+            turret.transform.localScale = new Vector2(0.4f, 0.4f);
+            bulletPrefab.transform.localScale = new Vector2(0.19f, 0.11f);
+            enemyPrefab.transform.localScale = new Vector2(0.28f, 0.28f);
+        }
     }
     public void AddScore(int scoreToAdd)
     {
